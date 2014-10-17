517661d6fe393cfb275646e7b19aefa6f1bcb589
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-10-17 12:07:44 +0200

    boletin 5.2

---
+++ src/boletin/pkg5/pkg2/Boletin52.java
@@ -0,0 +1,14 @@
+
+package boletin.pkg5.pkg2;
+
+
+public class Boletin52 {
+
+ 
+    public static void main(String[] args) {
+    satelite Satelite1= new satelite(25,43,57);
+    
+    Satelite1.verPosicion();
+    }
+    
+}
